6ab1c0bf354ff94085872d24586c91eef62bc440
olivarad <olivia.radecki02@gmail.com>
2022-12-06 03:06:59 -0500

    added pronouns to trainer data, will need continued work for formatting and secondary pronoun sets, also work on formatting hometowns

--- Pokemon.cc
+++ Pokemon.cc
@@ -67,6 +67,8 @@ class Trainer
             getline(cin, middle_name);
             cout << "Please enter your last name: ";
             getline(cin, last_name);
+            cout << "Please enter your pronouns: ";
+            getline(cin, pronouns);
         }
 
         void display_data()
@@ -79,25 +81,29 @@ class Trainer
                 {
                     cout << "Please choose the data to be displayed\n"; // Prompts for user choice
                     cout 
-                    << "1. Birthday\n"
-                    << "2. Hometown\n"
-                    << "3. Finance\n"
-                    << "4. Cancel\n";
+                    << "1. Pronouns\n"
+                    << "2. Birthday\n"
+                    << "3. Hometown\n"
+                    << "4. Finance\n"
+                    << "5. Cancel\n";
                     cin >> menu_choice;
-                } while(1 > menu_choice || menu_choice > 4);
+                } while(1 > menu_choice || menu_choice > 5);
                 switch(menu_choice)
                 {
-                    case 1: // Birthday
+                    case 1: // Pronouns
+                        cout << "\n   Pronouns: " << pronouns << endl << endl; // Displays pronouns
+                        break;
+                    case 2: // Birthday
                         cout << "\n   Birthday: " << birthday << endl << endl; // Displays the birthday
                         break;
-                    case 2: // Hometown
+                    case 3: // Hometown
                         cout << "\n   Hometown: " << hometown << endl << endl; // Displays the hometown
                         break;
-                    case 3: // Finance
+                    case 4: // Finance
                         cout << "\nPokedollars: " << pokedollars << endl << endl; // Displays finance
                         break;
                 }
-            } while (menu_choice != 4);
+            } while (menu_choice != 5);
         }
 
     private:
